0994d52ef9754de3a7e966a38b14a2dc5e577f9a
Insa <ilya2002i52@mail.ru>
2022-02-11 14:40:10 +0500

    xmlHttpRequest

--- js/script.js
+++ js/script.js
@@ -236,4 +236,62 @@ window.addEventListener('DOMContentLoaded', () => {
         '.menu .container',
         'menu__item'
     ).render();
+
+
+
+    // Forms
+
+    const forms = document.querySelectorAll('form');
+    console.log(forms);
+
+    const message = {
+        loading: 'Загрузка',
+        success: 'Спасибо, Скоро мы с вами свжемся',
+        failure: 'Smth went wrong'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+
+            request.setRequestHeader('Content-type', 'application/json');//'multipart/form-data'
+
+
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach((value, key)=>{
+                object[key] = value;
+            });
+            const json = JSON.stringify(object);
+
+            request.send(json);//formData
+
+            
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log((request.response));
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    },2000);
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+        });
+    }
 });
